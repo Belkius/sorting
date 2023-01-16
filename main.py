654b60f95c1f68c389137ad46d49bc6b98d2a2f8
Belkius <118485347+Belkius@users.noreply.github.com>
2023-01-16 19:01:54 +0100

    First upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#! python3.
+import numbers
+import algorithms
+import time
+
+
+a = []
+l = 10
+d = True
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Grzegorz')
+    numbers.generate(a, l, d)
+    a2 = a.copy()
+
+    start_time = time.time()
+    print("--- %s seconds ---" % (time.time() - start_time))
+    print(a)
+    algorithms.merge(a, l)
+    print(a)
+    print("--- %s seconds ---" % (time.time() - start_time))
+"""
+    start_time = time.time()
+    print("--- %s seconds ---" % (time.time() - start_time))
+    #print(a2)
+    algorithms.merge2(a2, l)
+    #print(a2)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+"""
